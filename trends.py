bfd4255c8f65a549cce97abdfb90de80dab6e4c3
Narenda Gollapilli <narendra.gollapilli@gmail.com>
2020-04-01 01:55:20 +0530

    Update trends.py

--- trends.py
+++ trends.py
@@ -1,6 +1,6 @@
 import pandas as pd                        
 from pytrends.request import TrendReq
-
+from keywords import KEYWORDS
 csvfile='red_blue.csv'
 
 def getstatas_csv():
@@ -8,8 +8,8 @@ def getstatas_csv():
 	Query for Red and Blue, generates CSV file
 	"""
 	pytrend = TrendReq()
-	kw_list = ['red', 'blue'] # queries to searcg
-	pytrend.build_payload(kw_list)
+	#kw_list = ['red', 'blue'] # queries to searcg
+	pytrend.build_payload(KEYWORDS)
 	data = pytrend.interest_over_time()
 	data.to_csv('red_blue.csv', encoding='utf_8_sig') # Dump trends to CSV file
 
@@ -22,5 +22,6 @@ def get_totalstats():
 	total_blue=df['blue'].sum()
 	total_red=df['red'].sum()
 	return (total_blue, total_red)
-
+print("***************TRENDS FOR KEYWORDS *****************")
+print("KEYWORDS", KEYWORDS )
 print (get_totalstats())
